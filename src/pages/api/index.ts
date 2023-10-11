ff385ffc70457fdbaab2842fc7b334e28f7a4d27
MiracleUFO <miracleufodiama@gmail.com>
2023-10-11 17:33:28 +0100

    chore: test if bruteforce is causing error

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,8 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { translate } from '@vitalets/google-translate-api';
-import ExpressBruteForce from 'express-brute';
-
-import store from '@/lib/mongoose';
 import enableCorsMiddleware from '@/lib/cors';
 
 export default async function handler(
@@ -17,15 +14,9 @@ export default async function handler(
   await enableCorsMiddleware(req, res);
 
   try {
-    if (store) {
-      //  brute force protection and rate limiting
-      const bruteForce = new ExpressBruteForce(store);
-      bruteForce.prevent;
-
-      const { text, from, to, fetchOptions } = req.body;
-      const translation = await translate(text as string, { from, to, fetchOptions });
-      return res.status(200).json(JSON.stringify(translation));
-    }
+    const { text, from, to, fetchOptions } = req.body;
+    const translation = await translate(text as string, { from, to, fetchOptions });
+    return res.status(200).json(JSON.stringify(translation));
   } catch (e) {
     console.error(e);
     return res.status(500).json(JSON.stringify(e));
